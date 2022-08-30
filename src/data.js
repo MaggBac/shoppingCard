478b6e29c94cda1964efc52f79046ece72715c0e
Magda <magda.bac77@gmail.com>
2022-08-30 12:42:44 +0200

    First commit

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = {
+	products: [
+		{
+			id: '1',
+			name: 'Figure ice skates',
+			price: 400,
+			image: 'https://cdn.pixabay.com/photo/2019/02/02/19/47/ice-skates-3971281_960_720.jpg',
+		},
+		{
+			id: '2',
+			name: 'Roller skates',
+			price: 100,
+			image: 'https://cdn.pixabay.com/photo/2018/03/30/20/27/skates-3276451_960_720.jpg',
+		},
+		{
+			id: '3',
+			name: 'Rollerblades',
+			price: 500,
+			image: 'https://cdn.pixabay.com/photo/2016/04/12/14/52/inline-skate-1324496_960_720.jpg',
+		},
+	],
+};
+export default data;
